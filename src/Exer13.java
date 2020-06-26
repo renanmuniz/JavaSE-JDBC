8fbee0a6a6627ee771dc64b873dbd93f29afdf47
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-26 17:17:42 -0300

    exer13

---
+++ src/Exer13.java
@@ -0,0 +1,42 @@
+import com.sun.rowset.JdbcRowSetImpl;
+
+import javax.sql.rowset.JdbcRowSet;
+import java.sql.*;
+
+public class Exer13 {
+    public static void main(String[] args) {
+        try{
+            Class.forName("org.hsqldb.jdbcDriver");
+            System.out.println("Driver carregado com sucesso.");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Erro ao carregar driver.");
+            e.printStackTrace();
+        }
+
+        try {
+            String url = "jdbc:hsqldb:file:D:/UniprimeWorkspaceIntelliJ/JavaSE-JDBC/hypersql/bases/aula;shutdown=true";
+            String usuario ="SA";
+            String senha = "1234";
+
+            JdbcRowSet rs = new JdbcRowSetImpl();
+            rs.setUrl(url);
+            rs.setUsername(usuario);
+            rs.setPassword(senha);
+
+            rs.setCommand("select * from cliente");
+            rs.execute();
+
+            while(rs.next())
+            {
+                System.out.println(rs.getInt("id") + " - " + rs.getString("nome")
+                        + " - " + rs.getString("email"));
+            }
+
+            rs.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
